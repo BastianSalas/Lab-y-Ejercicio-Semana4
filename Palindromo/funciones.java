17db466c3e3309d6bd71fbdb3263ca8238945e6a
BastianSalas <bastian.salas@cloud.uautonoma.cl>
2024-04-02 20:55:03 -0300

    listoo

--- Palindromo/funciones.java
+++ Palindromo/funciones.java
@@ -5,20 +5,20 @@ public class funciones {
 
     public static void verificarPalindromo() {
         System.out.print("Ingrese una palabra: ");
-        String palabra = scanner.next(); // Se solicita al usuario que ingrese una palabra y se guarda en la variable 'palabra'
+        String palabra = scanner.next();
     
-        if (esPalindromo(palabra.toLowerCase())) // Se llama al método 'esPalindromo()' para verificar si la palabra ingresada es un palíndromo
-            System.out.println("La palabra '" + palabra + "' es un palíndromo."); // Si es un palíndromo, se imprime un mensaje indicándolo
+        if (esPalindromo(palabra.toLowerCase()))
+            System.out.println("La palabra '" + palabra + "' es un palíndromo.");
         else
-            System.out.println("La palabra '" + palabra + "' no es un palíndromo."); // Si no es un palíndromo, se imprime un mensaje indicándolo
+            System.out.println("La palabra '" + palabra + "' no es un palíndromo.");
     }
     
     private static boolean esPalindromo(String palabra) {
-        if (palabra.length() <= 1) // Si la longitud de la palabra es menor o igual a 1, significa que es un palíndromo
+        if (palabra.length() <= 1)
             return true;
-        else if (palabra.charAt(0) == palabra.charAt(palabra.length() - 1)) // Si el primer y último carácter son iguales
-            return esPalindromo(palabra.substring(1, palabra.length() - 1)); // Se llama recursivamente a la función con la palabra sin el primer y último carácter
+        else if (palabra.charAt(0) == palabra.charAt(palabra.length() - 1))
+            return esPalindromo(palabra.substring(1, palabra.length() - 1));
         else
-            return false; // Si los caracteres no son iguales, la palabra no es un palíndromo
+            return false;
     }
 }
